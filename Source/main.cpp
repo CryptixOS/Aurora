6e634416279190b292c07e2a4bc0dcf67b0cbe36
V1tr10l7 <vitriol1744@gmail.com>
2025-09-08 22:56:02 +0200

    [refactor]: change return type of NeonMain to ErrorOr<void>

--- Source/main.cpp
+++ Source/main.cpp
@@ -10,182 +10,93 @@
 
 #include <Prism/Debug/Assertions.hpp>
 #include <Prism/Debug/Log.hpp>
+#include <Prism/Utility/Atomic.hpp>
 
+#include <cryptix/syscall.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <sys/mount.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <signal.h>
 
 using namespace Prism;
-void Info(const char* format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    Log::Logv(LogLevel::eInfo, format, args);
-    va_end(args);
-}
-void Trace(const char* format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    Log::Logv(LogLevel::eTrace, format, args);
-    va_end(args);
-}
-void OnError(const char* format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    Log::Logv(LogLevel::eError, format, args);
-    va_end(args);
-}
-void Message(const char* format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    Log::Logv(LogLevel::eNone, format, args);
-    va_end(args);
-}
+#define Assert(...) PrismAssert(__VA_ARGS__)
 
-constexpr usize MAX_LINE_LENGTH = 1024;
-constexpr usize MAX_FIELDS      = 6;
+#define DeclareLogNamed(name, level)                                           \
+    void name(const char* format, ...)                                         \
+    {                                                                          \
+        va_list args;                                                          \
+        va_start(args, format);                                                \
+        Log::Logv(LogLevel::e##level, format, args);                           \
+        va_end(args);                                                          \
+    }
+#define DeclareLog(level) DeclareLogNamed(level, level)
 
-struct MountPoint
+DeclareLog(Trace);
+DeclareLog(Info);
+DeclareLog(Debug);
+DeclareLog(Warn);
+DeclareLogNamed(Message, None);
+DeclareLogNamed(OnError, Error);
+
+ErrorOr<void> initializeStdIo()
 {
-    char* Source;         // Device or server
-    char* Target;         // Mount point
-    char* FilesystemType; // Filesystem type
-    char* Options;        // Mount options
-    i32   Frequency;      // Dump frequency
-    i32   FsckPassNumber; // Fsck pass number
-};
+    return {};
 
-void trimNewLine(char* str)
-{
-    usize len = strlen(str);
-    if (len && str[len - 1] == '\n') str[len - 1] = '\0';
-}
-usize parseMountOptions(char*& opts)
-{
-    usize flags = 0;
-    if (!opts) return 0;
-
-    char* options = strdup(opts);
-    char* token   = strtok(options, ",");
-
-    char* current = opts;
-    while (token)
+    i32 onefd = open("/dev/console", O_RDONLY, 0);
+    if (onefd != -1) dup2(onefd, 0);
+    i32 twofd = open("/dev/console", O_RDWR, 0);
+    if (twofd != -1)
     {
-        if (!strcmp(token, "ro")) flags |= MS_RDONLY;
-        else if (!strcmp(token, "noatime")) flags |= MS_NOATIME;
-        else if (!strcmp(token, "relatime")) flags |= MS_RELATIME;
-        else if (!strcmp(token, "nosuid")) flags |= MS_NOSUID;
-        else if (!strcmp(token, "nodev")) flags |= MS_NODEV;
-        else if (!strcmp(token, "noexec")) flags |= MS_NOEXEC;
-        else if (!strcmp(token, "sync")) flags |= MS_SYNCHRONOUS;
-        else if (!strcmp(token, "dirsync")) flags |= MS_DIRSYNC;
-        else strcpy(current, token);
-
-        current += strlen(token);
-        token = strtok(NULL, ",");
+        dup2(twofd, 1);
+        dup2(twofd, 2);
     }
 
-    delete options;
-    *current = 0;
-    return flags;
+    if (onefd > 2) close(onefd);
+    if (twofd > 2) close(twofd);
+
+    if (errno) return Error(errno);
+    return {};
+}
+ErrorOr<isize> mountFilesystems()
+{
+    int pid = fork();
+    if (pid == -1) return Error(errno);
+    else if (pid == 0)
+    {
+        execl("/bin/mount", "mount", "-a", nullptr);
+        OnError("Aurora: Failed to execute /bin/mount -a");
+        return Error(errno);
+    }
+
+    int status = -1;
+    waitpid(pid, &status, 0);
+
+    if (errno) return Error(errno);
+    return status;
 }
 
-i32 parseMountPoint(const char* line, MountPoint& entry)
+namespace mlibc
 {
-    char* fields[MAX_FIELDS] = {0};
-    char* temp               = strdup(line);
-    char* token              = strtok(temp, " \t");
-    i32   fieldCount         = 0;
-
-    while (token && fieldCount < static_cast<i32>(MAX_FIELDS))
-    {
-        fields[fieldCount++] = token;
-        token                = strtok(NULL, " \t");
-    }
-
-    if (fieldCount < 4)
-    {
-        delete temp;
-        return 0;
-    }
-
-    entry.Source         = strdup(fields[0]);
-    entry.Target         = strdup(fields[1]);
-    entry.FilesystemType = strdup(fields[2]);
-    entry.Options        = strdup(fields[3]);
-    entry.Frequency      = (fieldCount >= 5) ? atoi(fields[4]) : 0;
-    entry.FsckPassNumber = (fieldCount >= 6) ? atoi(fields[5]) : 0;
-
-    delete temp;
-    return 1;
-}
-
-void freeMountPoint(MountPoint& entry)
-{
-    delete entry.Source;
-    delete entry.Target;
-    delete entry.FilesystemType;
-    delete entry.Options;
-}
-
-isize mountFilesystems()
-{
-    FILE* file = fopen("/etc/fstab", "r");
-    if (!file)
-    {
-        OnError("Failed to open /etc/fstab");
-        return EXIT_FAILURE;
-    }
-
-    char line[MAX_LINE_LENGTH];
-    i32  lineNumber = 0;
-
-    while (fgets(line, sizeof(line), file))
-    {
-        lineNumber++;
-        trimNewLine(line);
-
-        // Skip comments and empty lines
-        if (line[0] == '#' || strlen(line) == 0) continue;
-
-        MountPoint entry;
-        if (parseMountPoint(line, entry))
-        {
-            usize mountFlags = parseMountOptions(entry.Options);
-            Trace("Aurora: Mount options: %s", entry.Options);
-            i32 mountStatus
-                = mount(entry.Source, entry.Target, entry.FilesystemType,
-                        mountFlags, entry.Options);
-            Trace("Aurora: Mount status: %d", mountStatus);
-            if (mountStatus == -1)
-                OnError(
-                    "Aurora: Failed to mount `%s` filesystem at `%s`, "
-                    "source: %s, flags: `%s`\nerror code: %s",
-                    entry.FilesystemType, entry.Target, entry.Source,
-                    entry.Options, strerror(errno));
-
-            freeMountPoint(entry);
-        }
-        else
-            OnError("Aurora: Skipping invalid or incomplete line %d",
-                    lineNumber);
-    }
-
-    fclose(file);
-    return EXIT_SUCCESS;
+    extern void sys_sigreturn();
 }
 
 static void signalHandler(int signo)
 {
-    Info("Aurora: Received %d signal", signo);
+    Info("Aurora: Received %ld signal", i64(signo));
 }
 
-int NeonMain(const Vector<StringView>& argv, const Vector<StringView>& envp)
+ErrorOr<void> NeonMain(const Vector<StringView>& argv,
+                       const Vector<StringView>& envp)
 {
+    Trace("Aurora: Ininitializing...");
+    Debug("Aurora: Arguments => ");
+    for (usize i = 0; const auto arg : argv)
+        Message("\tArgs[%zu]: %s", i++, arg);
+    Debug("Aurora: Environment variables => ");
+    for (usize i = 0; const auto env : envp)
+        Message("\tEnvs[%zu]: %s", i++, env);
+
     Trace("Aurora: Setting up environment variables");
     using namespace Neon;
 
@@ -195,15 +106,17 @@ int NeonMain(const Vector<StringView>& argv, const Vector<StringView>& envp)
     Environment::Overwrite("HOME", "/root");
     Environment::Overwrite("PATH", "/usr/local/bin:/ur/bin:/usr/sbin");
 
-    Message("Aurora: Welcome to CryptixOS!");
+    Message("\n\n\n\n");
+    Info("Aurora: Welcome to CryptixOS!");
 #endif
 
+    Assert(initializeStdIo());
     mountFilesystems();
     static constexpr PathView shellPath = "/usr/bin/bash"_sv;
     if (!Filesystem::Access(shellPath, FileMode::eExecute))
     {
         OnError("Aurora: Failed to access the shell => %s", shellPath);
-        return EXIT_FAILURE;
+        return Error(errno);
     }
 
     struct sigaction psa;
@@ -211,6 +124,7 @@ int NeonMain(const Vector<StringView>& argv, const Vector<StringView>& envp)
     sigaction(SIGHUP, &psa, nullptr);
 
     pid_t self = getpid();
+    Trace("Aurora: Sending %d signal to pid #%d", SIGHUP, self);
     kill(self, SIGHUP);
 
     for (;;)
@@ -221,7 +135,7 @@ int NeonMain(const Vector<StringView>& argv, const Vector<StringView>& envp)
         if (pid == -1)
         {
             OnError("Aurora: fork failed");
-            return EXIT_FAILURE;
+            return Error(errno);
         }
         else if (pid == 0)
         {
@@ -245,5 +159,5 @@ int NeonMain(const Vector<StringView>& argv, const Vector<StringView>& envp)
     }
 
     Trace("Aurora: Exiting...");
-    return EXIT_SUCCESS;
+    return {};
 }
